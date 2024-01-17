8ca7e357e3099f1967568258fb5fa03238456760
NihaalAftab <NihaalAftab@gmail.com>
2024-01-17 17:38:21 +0500

    delete everything

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import { WikiResultProps } from "../types"
-
-const wikiUrl = "https://en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srsearch="
-
-export const searchWiki = async (text: string, offset: number): Promise<WikiResultProps> => {
-    return fetch(`${wikiUrl}${text}&sroffset=${offset}`).then((res) => res.json())
-}
